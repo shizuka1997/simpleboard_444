cd63da2efe75167506c70ae65655ed6d0c081a7c
shizuka1997 <system.study.1997@gmail.com>
2022-05-08 14:29:51 +0900

    header error commit

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -5,10 +5,13 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        @component('components.header')
+        @endcomponent
         <div class="container">
             @yield('content')
         </div>
 
-         <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
-</html> 
+</html>
+
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,14 @@
-@if (session('message'))
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+    @if (session('message'))
         {{ session('message') }}
     @endif
 
     {{ $post->title }}
-    {{ $post->content }} 
+    {{ $post->content }}
+@endsection
 
     <a href="/posts/{{ $post->id }}/edit">Edit</a>
-    
