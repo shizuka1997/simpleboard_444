ff3d24f0c6ebcb198e77e05be21b52d80b9a1ad9
shizuka1997 <system.study.1997@gmail.com>
2022-05-10 20:08:26 +0900

    fix posts layouts

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -11,7 +11,10 @@
             @yield('content')
         </div>
 
+        @component('components.footer')
+        @endcomponent
+
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
-</html>
+</html> 
 
